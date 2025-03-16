bb451df5dbbd38ac6216d241c5292dfc2a4632ce
Manan Gupta <manangupta1357@gmail.com>
2025-03-16 11:42:02 +0530

    chunk

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true })
+  ],
+  build: {
+    chunkSizeWarningLimit: 600,
+  },
 })
+
+const loadComponent = async () => {
+    const module = await import('src/components/Cursor.jsx');
+    return module.default;
+};
